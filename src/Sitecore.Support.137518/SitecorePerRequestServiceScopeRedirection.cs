0d3d20766e7a5dea0aa86481437921d05e05bb06
Dmitry Kolinchuk <dmko@sitecore.net>
2016-12-06 17:51:49 +0200

    Prevents from “OutOfMemoryException” when using “Sitecore.DependencyInjection.ServiceLocator” too many times

---
+++ src/Sitecore.Support.137518/SitecorePerRequestServiceScopeRedirection.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.DependencyInjection;
+using System;
+
+namespace Sitecore.Support.DependencyInjection
+{
+    public class SitecorePerRequestServiceScopeRedirection : ISitecoreServiceLocatorScope
+    {
+        IServiceProvider _applicationServiceProvider;
+
+        public SitecorePerRequestServiceScopeRedirection(IServiceProvider applicationServiceProvider)
+        {
+            _applicationServiceProvider = applicationServiceProvider;
+        }
+
+        public IServiceProvider ServiceProvider
+        {
+            get
+            {
+                var scope = SitecorePerRequestScopeModule.GetScope(_applicationServiceProvider);
+                return scope != null ? scope.ServiceProvider : _applicationServiceProvider;
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
